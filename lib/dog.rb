68d858895b0e75401f3901a7e16f1b61b0101146
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-14 18:32:19 -0400

    not completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,61 @@
 class Dog
+    attr_accessor :name, :breed
+    attr_reader :id
+
+    def initialize(id: nil, name:, breed:)
+        @id = id
+        @name = name
+        @breed = breed
+    end
+
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
+
+
+    def self.drop_table
+        sql = "DROP TABLE IF EXISTS dogs"
+        DB[:conn].execute(sql)
+    end
+
+
+    def save
+        if self.id
+            self
+        else
+            sql = <<-SQL
+                INSERT INTO dogs (name, breed)
+                VALUES (?, ?)
+            SQL
+            DB[:conn].execute(sql, self.name, self.breed)
+            @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+            self
+        end 
+    end
+
+
+    def self.create(hash)
+        dog = Dog.new(hash)
+        dog.save
+        dog
+    end
+
+
+    def self.new_from_db(row)
+        id = row[0]
+        name = row[1]
+        breed = row[2]
+        Dog.new(id, name, breed)
+
+    end
+
 
 end
